485182617bda54e57582720851efb9b274461a19
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2022-09-14 20:55:26 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -24,10 +24,17 @@ agreement.addEventListener('click', unchecked);
 unchecked();
 
 const counter = document.getElementById('counter');
-const textArea = document.getElementById('feedback');
+const textArea = document.getElementById('textarea');
 
 textArea.addEventListener('input', (e) => {
   const caracter = e.target;
   const count = 500 - caracter.value.length;
   counter.innerText = count;
+});
+
+submitBtn.addEventListener('click', () => {
+  let formResults = document.querySelector('#evaluation-form');
+  for (let i = 0; i < formResults.length; i += 1) {
+    formResults += formResults.elements[i].value + '<br>';
+  }
 });
