186b5d89d285a8777ccb784ace5a7dde5a3d62ab
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2022-09-14 18:20:38 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const loginBtn = document.querySelector('#login-button');
 const emailInput = document.querySelector('#email-input');
 const passwordInput = document.querySelector('#password-input');
+const agreement = document.querySelector('#agreement');
+const submitBtn = document.querySelector('#submit-btn');
 
 loginBtn.addEventListener('click', () => {
   if (emailInput.value === 'tryber@teste.com' && passwordInput.value === '123456') {
@@ -9,3 +11,14 @@ loginBtn.addEventListener('click', () => {
     alert('Email ou senha inválidos.');
   }
 });
+
+const unchecked = () => {
+  if (agreement.checked === true) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+};
+
+agreement.addEventListener('click', unchecked);
+unchecked();
